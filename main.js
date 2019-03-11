11c9f9047bbec004dd2cfabe429c96ddc155852c
teerapatpor <35361062+teerapatpor@users.noreply.github.com>
2019-03-11 16:30:09 +0700

    change first time

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log('Hello World');
 var str = '';
 for(var i=0; i<10; i++){
     console.log(i+1);
-    str += (i+1) + '<br>';
+    str += (i+3) + '<br>';
 }
 
 document.querySelector("#outputConsole").innerHTML = str;
